58bd5887af5424b0b39002ed110db34fde5fa84c
Jonah <jonahkf@gmail.com>
2024-09-17 11:57:58 +0900

    REMOVED: Nested for loop
    No need for nesting the grid creation, as rows
    are determined by a flex-basis percentage.

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 //Initialize Grid
-function setGrid(){
-    const container = document.querySelector("#container");
-    for(let i = 0; i < 16; i++){
-        for(let e = 0; e < 16; e++){
-            const div = document.createElement("div");
-            div.setAttribute("id", "child");
+function setGrid() {
+  const container = document.querySelector("#container");
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", "child");
 
-            div.addEventListener('mouseenter',() => {
-                div.setAttribute("style", "background-color: black;");
-            });
+    div.addEventListener("mouseenter", () => {
+      div.setAttribute("style", "background-color: black;");
+    });
 
-            container.appendChild(div);
-        }
-    }
+    container.appendChild(div);
+  }
 }
-
-
 
 //Start Etch-a-Sketch
 setGrid();
