d74d1dba432cd49d14aa6882117fc36e33435aba
Jonah <jonahkf@gmail.com>
2024-09-17 11:29:28 +0900

    INITIAL COMMIT: Etch-a-Sketch Project
    Created index, scrip, styles files.
    Added HTML boilerplate, linking to js and css.
    Used nested for loops to make 16x16 div grid.
    Grid scales to window width, but not height.

---
+++ script.js
@@ -0,0 +1,14 @@
+//Initialize Grid
+function setGrid(){
+    const container = document.querySelector("#container");
+    for(let i = 0; i < 16; i++){
+        for(let e = 0; e < 16; e++){
+            const div = document.createElement("div");
+            div.setAttribute("id", "child");
+            container.appendChild(div);
+        }
+    }
+}
+
+//Start Etch-a-Sketch
+setGrid();
